da504006def562ba7a9b7fdf39b4c5c726dfb244
TheThomaas <thethomaas@protonmail.ch>
2026-09-16 16:21:28 +0200

    Update cookcli-card.js

--- dist/cookcli-card.js
+++ dist/cookcli-card.js
@@ -58,8 +58,7 @@ class CookCliCard extends HTMLElement {
     return {
       schema: [
         { name: "title", selector: { text: {} }, label: "Titre" },
-        //{ name: "dashboard_path", selector: { text: {} }, label: "Chemin du dashboard", helper: "Laisser vide pour le dashboard courant" },
-        { name: "dashboard_path", selector: { navigation_path: {} }, label: "Chemin du dashboard", helper: "Laisser vide pour le dashboard courant" },
+        { name: "dashboard_path", selector: { text: {} }, label: "Chemin du dashboard", helper: "Laisser vide pour le dashboard courant" },
         { name: "entry_id", selector: { text: {} }, label: "Entry ID", helper: "Optionnel — si plusieurs serveurs CookCLI" },
       ],
     };
